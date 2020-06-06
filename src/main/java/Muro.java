1d821432ecfe7d8b7392580811336072c3a18eab
Jose María <jmcr2500@gmail.com>
2020-06-06 20:18:12 +0200

    Commit 06 Eliminada duplicidad de codigo de Muro

--- src/main/java/Muro.java
+++ src/main/java/Muro.java
@@ -1,26 +1,14 @@
 import java.util.ArrayList;
 
-/**
- * El muro de nuestra red social.
- *
- * El muro es el lugar donde los usuarios publicaran sus entradas.
- *
- * @author DAM
- * @version 1.0
- */
 public class Muro
 {
     // Entradas de texto
     private ArrayList <Entrada> entradas;
 
-    /**
-     * Constructor - Construye e inicializa un muro vacio.
-     */
     public Muro() {
         //Inicializa mensajes y fotos.
         entradas = new ArrayList<>();
     }
-
 
     public void addEntrada(Entrada entrada) {
         entradas.add(entrada);
